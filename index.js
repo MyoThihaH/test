9e198df7d6eadc441f72574c29cfc0f5f890677c
Myo_Thiha_Htay <myothihahtay@viabells.com>
2024-12-22 10:55:38 +0630

    update

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import * as path from "path";
 
 import listRoute from "./routes/listRoutes/listRoute.js";
 import bodyParser from "body-parser";
 const app = express();
 
-const PORT = process.env.PORT | 4000;
+const PORT = process.env.PORT | 4001;
 
 const CONNECTION_URL =
   "mongodb+srv://admin:8Fs18L5udvgGWBs7@cluster0.r9qov.mongodb.net/Test?retryWrites=true&w=majority";
-app.use(express.static(path.resolve("./build")));
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: false }));
 app.use("/lists", listRoute);
